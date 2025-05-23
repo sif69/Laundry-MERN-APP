f692c9d889c66a73e9028d6d92b1882e0691ef18
sif69 <sifat.sust.cse20@gmail.com>
2025-05-23 13:50:21 +0600

    added : Pages | Components | Headers | Footers | Layouts

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () =>{
+
+    return(
+        <div className='bg-dark text-light text-center p-3'>
+        <h4>
+            All rights reserved &copy; SUST_CSE_2020
+        </h4>
+        </div>         
+    )
+}
+export default Footer;
