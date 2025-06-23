6e57b2ac16029aeefa9fcdf76dbcd176651be6e5
Mohammad Noyeem uddin sifat <107163779+sif69@users.noreply.github.com>
2025-06-23 10:23:59 +0600

    Update AdminLogin.jsx

--- frontend/src/pages/Auth/AdminLogin.jsx
+++ frontend/src/pages/Auth/AdminLogin.jsx
@@ -19,7 +19,7 @@ const AdminLogin = () => {
     e.preventDefault();
     try {
       //host -/api/v1/auth/login
-      const res = await axios.post("https://laundry-mern-app.onrender.com/api/v1/auth/login", {
+      const res = await axios.post("/api/v1/auth/login", {
         email,
         password,
       });
@@ -34,9 +34,9 @@ const AdminLogin = () => {
         if (res.data.user.role === 1) {
           navigate("/dashboard/admin");
         } else if (res.data.user.role === 2) {
-          navigate("https://laundry-mern-app.onrender.com/dashboard/user/orders");
+          navigate("/dashboard/user/orders");
         } else {
-          navigate("https://laundry-mern-app.onrender.com/dashboard/user");
+          navigate("/dashboard/user");
         }
       } else {
         toast.error(res.data.message);
