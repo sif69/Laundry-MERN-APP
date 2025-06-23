e17e1ba6561283f1c0b85e24be2e3353770e2743
Mohammad Noyeem uddin sifat <107163779+sif69@users.noreply.github.com>
2025-06-23 14:06:47 +0600

    Update CreateCategory.jsx

--- frontend/src/pages/Admin/CreateCategory.jsx
+++ frontend/src/pages/Admin/CreateCategory.jsx
@@ -15,7 +15,7 @@ const CreateCategory = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const { data } = await axios.post("https://laundry-mern-app-backend.onrender.com/api/v1/category/create-category", {
+      const { data } = await axios.post("/api/v1/category/create-category", {
         name,
       });
       if (data?.success) {
@@ -33,7 +33,7 @@ const CreateCategory = () => {
   //get all service category
   const getAllCategory = async () => {
     try {
-      const { data } = await axios.get("https://laundry-mern-app-backend.onrender.com/api/v1/category/get-category");
+      const { data } = await axios.get("/api/v1/category/get-category");
       if (data.success) {
         setCategories(data.category);
       }
@@ -52,7 +52,7 @@ const CreateCategory = () => {
     e.preventDefault();
     try {
       const { data } = await axios.put(
-        `https://laundry-mern-app-backend.onrender.com/api/v1/category/update-category/${selected._id}`,
+        `/api/v1/category/update-category/${selected._id}`,
         { name: updatedName }
       );
       if (data.success) {
@@ -72,7 +72,7 @@ const CreateCategory = () => {
   const handleDelete = async (pid) => {
     try {
       const { data } = await axios.delete(
-        `https://laundry-mern-app-backend.onrender.com/api/v1/category/delete-category/${pid}`,
+        `/api/v1/category/delete-category/${pid}`,
         name
       );
       if (data.success) {
