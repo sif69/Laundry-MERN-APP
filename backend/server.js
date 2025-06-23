33a0faaf1329e214c8cf7360b4781717541f8347
sif69 <sifat.sust.cse20@gmail.com>
2025-06-23 18:40:02 +0600

    cart large payload handle

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,8 @@ const app = express();
 app.use("/uploads/profile", express.static("uploads/profile"));
 // middlewares
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.json({limit : "20mb"}));
+app.use(express.urlencoded({ extended: true,limit:"20mb" })); 
 app.use(morgan('dev'));
 
 
